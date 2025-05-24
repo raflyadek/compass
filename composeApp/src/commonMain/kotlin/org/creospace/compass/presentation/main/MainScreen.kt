39c7458ef9bfb24b3ff274f0eddf28c773310b75
Rafly <raflyade9@gmail.com>
2025-05-24 19:21:33 +0700

    MainScreen: add new state logic

--- composeApp/src/commonMain/kotlin/org/creospace/compass/presentation/main/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/org/creospace/compass/presentation/main/MainScreen.kt
@@ -1,9 +1,13 @@
 package org.creospace.compass.presentation.main
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -22,18 +26,29 @@ import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.input.key.Key.Companion.R
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import compass.composeapp.generated.resources.Res
+import compass.composeapp.generated.resources.group_6
+import compass.composeapp.generated.resources.image_group_6
+import compass.composeapp.generated.resources.image_main
+import compass.composeapp.generated.resources.image_main_1
 import org.creospace.compass.data.Journal
 import org.creospace.compass.presentation.Screens
 import org.creospace.compass.presentation.components.ItemJournal
 import org.creospace.compass.presentation.components.Spacer
 import org.creospace.compass.presentation.components.TextMedium
 import org.creospace.compass.theming.primaryLight
+import org.jetbrains.compose.resources.imageResource
+import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.viewmodel.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -65,15 +80,35 @@ fun MainScreen(navController: NavController, viewModel: MainViewModel = koinView
                     .padding(16.dp),
                 fontSize = 24
             )
+            if (journals.isNotEmpty()) {
+                LazyColumn {
+                    items(journals.size) { index ->
+                        val journal = journals[index]
+                        ItemJournal(
+                            title = journal.title.orEmpty(),
+                            description = journal.description.orEmpty(),
+                            toDetail = { toDetail(journal) }
+                        )
+                    }
+                }
+            } else {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center
 
-            LazyColumn {
-                items(journals.size) { index ->
-                    val journal = journals[index]
-                    ItemJournal(
-                        title = journal.title.orEmpty(),
-                        description = journal.description.orEmpty(),
-                        toDetail = {toDetail(journal)}
-                    )
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .wrapContentSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(
+                            painter = painterResource(Res.drawable.image_group_6),
+                            contentDescription = ""
+                        )
+                    }
                 }
             }
         }
