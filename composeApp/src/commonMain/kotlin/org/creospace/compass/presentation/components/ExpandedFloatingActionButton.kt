379d0902ed4bb2899c697d8486ab0977f9066d18
Rafly <raflyade9@gmail.com>
2025-05-25 22:11:03 +0700

    chore: edit the expanded fab

--- composeApp/src/commonMain/kotlin/org/creospace/compass/presentation/components/ExpandedFloatingActionButton.kt
+++ composeApp/src/commonMain/kotlin/org/creospace/compass/presentation/components/ExpandedFloatingActionButton.kt
@@ -10,13 +10,17 @@ import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -25,7 +29,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -44,18 +50,19 @@ fun ExpandedFloatingActionButton(
     var expanded by remember { mutableStateOf(false) }
     val items = listOf(
         MiniFabItems(
-            Icons.Filled.Edit,
-            "Edit",
-            onClick = {
-                navController.navigate(Screens.CreateJournal.route)
-            }
-        ),
-        MiniFabItems(
-            Icons.Filled.Delete,
+            Icons.Outlined.Delete,
             "Delete",
             onClick = {
                 deleteClick()
             }
+        ),
+        MiniFabItems(
+            Icons.Outlined.Edit,
+            "Edit",
+            onClick = {
+                navController.navigate(Screens.CreateJournal.route)
+            },
+//            color = MaterialTheme.colorScheme.primary
         )
     )
     Column(horizontalAlignment = Alignment.End) {
@@ -67,7 +74,7 @@ fun ExpandedFloatingActionButton(
             LazyColumn(Modifier.padding(bottom = 8.dp)) {
                 items(items.size) {
                     ItemFloatingActionButton(icon = items[it].icon, title = items[it].title, onClick = items[it].onClick)
-                    Spacer(8.dp)
+                    Spacer(2.dp)
                 }
             }
         }
@@ -92,16 +99,22 @@ fun ExpandedFloatingActionButton(
 @Composable
 fun ItemFloatingActionButton(modifier: Modifier = Modifier, icon: ImageVector, title: String, onClick: () -> Unit) {
     ExtendedFloatingActionButton(
+        modifier = Modifier
+            .clip(RoundedCornerShape(50.dp))
+        ,
         onClick = {
             onClick()
         },
+//        containerColor =
     ) {
-        Text(text = title)
         Icon(imageVector = icon, contentDescription = "")
+        Spacer(2.dp)
+        Text(text = title)
     }
 }
 data class MiniFabItems(
     val icon: ImageVector,
     val title: String,
-    val onClick: () -> Unit
+    val onClick: () -> Unit,
+//    val color: Color
 )
